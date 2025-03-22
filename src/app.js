e8de6319d0fba717299a8b3c04ca561183e32371
Tushar Vashihst <tusharvashisht9905@gmail.com>
2025-03-22 22:32:37 +0530

    Playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,10 +2,27 @@ const express = require('express');
 
 const app = express();
 
-app.use((req,res)=>{
-    res.send("Namaste from the server");
-})
+app.get('/user',(req,res)=>{
+    // console.log(req.query);
+    res.send({firstName:"Tushar",lastName:"Vashisht"});
+});
+
+// app.post("/user",(req,res)=>{
+//     res.send("Data successfully saved to the database");
+// })
+
+// app.delete("/user",(req,res)=>{
+//     res.send("Delete successfully");
+// })
 
 app.listen(7777, ()=>{
     console.log("Server is successfully running on port 7777...");
 }); 
+
+// app.use("/",(req,res)=>{
+//     res.send("Namaste from the server");
+// })
+
+// app.use("/hello",(req,res)=>{
+//     res.send("hello hello hello");
+// })
