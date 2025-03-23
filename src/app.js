b2d9d1f20420c5b7f08d14337b426cb374127b0f
Tushar Vashihst <tusharvashisht9905@gmail.com>
2025-03-23 11:03:24 +0530

    Writing-auth-middleware

--- src/app.js
+++ src/app.js
@@ -2,15 +2,51 @@ const express = require('express');
 
 const app = express();
 
-app.use(
-    "/user",
-    (req,res,next)=>{
-    res.send("Route Handler 1");
-    // next();
-},
-(req,res)=>{
-    res.send("Route Handler 2");
+const {adminAuth , userAuth} =require("./middlewares/auth");
+//Handle Auth Middlewares for all GET ,POST ,... requests
+app.use("/admin",adminAuth);
+
+app.get("/user",userAuth,(req,res)=>{
+    res.send("User Data Send");
+})
+app.get("/admin/getAllData",(req,res)=>{
+    res.send("All Data Send");
 });
+
+app.get("/admin/deleteUser",(req,res)=>{
+    res.send("Deleted a User");
+});
+// app.get("/admin/getAllData",(req,res)=>{
+//     const token="xyz";
+//     const isAdminAuthorized = token === "xyz";
+//     if(isAdminAuthorized){
+//         res.send("All Data Send");
+//     }
+//     else{
+//         res.status(401).send("Unauthorized request");
+//     }
+// });
+
+// app.get("/admin/delete User",(req,res)=>{
+//     const token="xyz";
+//     const isAdminAuthorized = token === "xyz";
+//     if(isAdminAuthorized){
+//         res.send("Deleted a User");
+//     }
+//     else{
+//         res.status(401).send("Unauthorized request");
+//     }
+// });
+
+// app.use(
+//     "/user",
+//     (req,res,next)=>{
+//     res.send("Route Handler 1");
+//     // next();
+// },
+// (req,res)=>{
+//     res.send("Route Handler 2");
+// });
 
 app.listen(7777, ()=>{
     console.log("Server is successfully running on port 7777...");
