8272cd148f8370eddc8d109ad95c8a0fa9d1ffa5
Tushar Vashihst <tusharvashisht9905@gmail.com>
2025-03-25 17:18:31 +0530

    schema validation

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const app = express();
 const User =require("./models/user");
 
 app.use(express.json());
+
 app.post("/signup", async(req,res)=>{
     const user = new User(req.body);
     try{
@@ -11,6 +12,60 @@ app.post("/signup", async(req,res)=>{
     res.send("User added successfully");
     }catch(err){
         res.status(400).send("Error saving the user"+err.message);
+    }
+});
+
+//GET user by email
+app.get("/user",async(req,res)=>{
+    const userEmail=req.body.emailId;
+
+    try{
+        const users = await User.find({emailId:userEmail});
+        if(users.length===0){
+            res.status(400).send("User not found");
+        }
+        else{
+            res.send(users);
+        }
+        
+    }
+    catch(err){
+        res.status(400).send("Error fetching email");
+    }
+})
+
+//get all the users from the database
+app.get("/feed",async(req,res)=>{
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }catch(err){
+        res.status(400).send("Something went wrong");
+    }
+});
+
+//delete user from the database
+app.delete("/user",async(req,res)=>{
+    const userId=req.body.userId;
+    try{
+        const user= await User.findByIdAndDelete(userId);
+        res.send("user deleted successfully");
+    }catch(err){
+        res.status(400).send("Something went wrong");
+    }
+});
+
+//update data of the user
+app.patch("/user",async(req,res)=>{
+    const userId=req.body.userId;
+    const data=req.body;
+    try{
+        await User.findByIdAndUpdate({_id:userId},data,{
+            runValidators:true,
+        });
+        res.send("data updated successfully");
+    }catch(err){
+        res.status(400).send("UPDATE FAILED "+err.message);
     }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -2,24 +2,53 @@ const mongoose=require("mongoose");
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required:true,
+        minLength:4,
+        maxLength:50,
     },
     lastName:{
         type: String
     },
     emailId:{
-        type: String
+        type: String,
+        lowercase:true,
+        trim:true,
+        required:true,
+        unique:true,
     },
     password:{
-        type: String
+        type: String,
+        required:true,
     },
     age:{
-        type: Number 
+        type: Number,
+        min:18,
     },
     gender:{
-        type: String
-    }
-});
+        type: String,
+        validate(value){
+            if(!["male" , "female" ,"others"].includes(value)){
+                throw new Error("Gender data is not valid");
+            }
+        }
+    },
+    photoUrl:{
+        type: String,
+        default:"https://www.pnrao.com/wp-content/uploads/2023/06/dummy-user-male.jpg"
+    },
+    about:{
+        type: String,
+        default:"This is the default value of the user."
+    },
+    skills:{
+        type: [String]
+    },
+},
+{
+    timestamps:true
+}
+);
 
 
 module.exports = mongoose.model("User",userSchema);;
