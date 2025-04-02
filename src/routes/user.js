e9e170834db7b9011162d4692ca5e3a9fae9ae4a
Tushar Vashihst <tusharvashisht9905@gmail.com>
2025-04-02 14:56:12 +0530

    make api

--- src/routes/user.js
+++ src/routes/user.js
@@ -52,6 +52,11 @@ userRouter.get("/feed",userAuth,async(req,res)=>{
     try{
         const loggedInUser = req.user;
 
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit>50 ? 50:limit;
+        const skip = (page-1)*limit;
+
         const connectionRequests = await ConnectionRequest.find({
             $or:[{fromUserId:loggedInUser._id},{toUserId:loggedInUser._id}],
         }).select("fromUserId toUserId");
@@ -67,9 +72,9 @@ userRouter.get("/feed",userAuth,async(req,res)=>{
             {_id:{$nin: Array.from(hideUsersFromFeed)}},
             {_id:{$ne : loggedInUser._id}},
             ],
-        }).select(USER_SAFE_DATA);
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit);
 
-        res.send(users);
+        res.json({data:users});
     }catch(err){
         res.status(400).send("ERROR: "+err.message);
     }
